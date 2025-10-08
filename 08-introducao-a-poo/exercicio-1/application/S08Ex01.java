d3eeb1f3e71280d62ae629fc5e2cf88e025dafec
Thiago Alves <thiagoalvs10@gmail.com>
2025-10-08 15:34:45 -0300

    Adiciona exercícios S08 resolvidos

---
+++ 08-introducao-a-poo/exercicio-1/application/S08Ex01.java
@@ -0,0 +1,27 @@
+/* Fazer um programa para ler os valores da largura e altura de um retângulo.
+Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.
+Usar uma classe como mostrado no projeto ao lado. */
+
+package application;
+
+import entities.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class S08Ex01 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", rectangle.Area());
+        System.out.printf("PERIMETER = %.2f%n", rectangle.Perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", rectangle.Diagonal());
+
+        sc.close();
+    }
+}
---
+++ 08-introducao-a-poo/exercicio-3/application/S08Ex03.java
@@ -0,0 +1,34 @@
+/* Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada).
+Ao final, mostrar qual a nota final do aluno no ano. Dizer também se o aluno está aprovado (PASS) ou não (FAILED)
+e, em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado (que é 60% da nota).
+Você deve criar uma classe Student para resolver este problema. */
+
+package application;
+
+import entities.Student;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class S08Ex03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+
+        student.name = sc.nextLine();
+        student.n1 = sc.nextDouble();
+        student.n2 = sc.nextDouble();
+        student.n3 = sc.nextDouble();
+
+        if (student.finalGrade() < 60.0) {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS%n", student.missingPoints());
+        }
+        else {
+            System.out.println("PASS");
+        }
+
+        sc.close();
+    }
+}
