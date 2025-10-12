46d01f418274f61e8fb88a9f686dc08daae0c15e
Thiago Alves <thiagoalvs10@gmail.com>
2025-10-12 00:11:10 -0300

    Adiciona exercício S09 resolvido

---
+++ 09-construtores-e-encapsulamento/src/application/S09Banco.java
@@ -0,0 +1,49 @@
+package application;
+
+import entities.Account;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class S09Banco {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Account account;
+
+        System.out.print("Enter account number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Is there a initial deposit (y/n)? ");
+        char response = sc.next().charAt(0);
+
+        if (response == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            account = new Account(number, holder, initialDeposit);
+        } else {
+            account = new Account(number, holder);
+        }
+
+        System.out.println();
+        System.out.print("Account data: ");
+        System.out.print(account);
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double depositValue = sc.nextDouble();
+        account.deposit(depositValue);
+        System.out.print("Updated account data: ");
+        System.out.print(account);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdrawValue = sc.nextDouble();
+        account.withdraw(withdrawValue);
+        System.out.print("Updated account data: ");
+        System.out.print(account);
+
+        sc.close();
+    }
+}
